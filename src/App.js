a99e0f73f1be8252673a509858672aa280507616
keith <xqk559@gmail.com>
2020-01-23 10:19:31 -0500

    First commit renders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor (props){
+    super ();
+  }
+  render (){
+
+    return (
+      <div>
+        <br />
+        <br />
+        <div className="rainbow-text">
+          Habitual
+        </div>
+        <div className="rainbow-text-small">
+          You've done  many things today!
+        </div>
+        <div className="bigmargin">
+        <ItemList />
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        </div>
+        <ItemCount count={allTheThings.length} />
+        <hr />
+        <AppFooter />
+      </div>
+    );
+  }
 }
+
+
+
+
+class Item extends React.Component {
+  constructor (props){
+    super ();
+
+    this.state = {
+      checked: false,
+      quantityChecked: 0
+    };
+
+    this.handleClick = this.handleClick.bind(this);    
+  }
+  handleClick (e){
+    this.setState({
+      checked: !this.state.checked
+    });
+
+    this.setState({
+      quantityChecked: this.state.quantityChecked + 1 
+    });  
+
+  }
+
+  
+
+  render (){
+    let text = this.state.checked ? <strike>{this.props.message}</strike> : this.props.message;
+    return (
+        <div className="row">
+          <div className="col-md-12">
+            <input type="checkbox" onClick={this.handleClick} />&nbsp;{text}
+            <hr />
+          </div>
+        </div>
+    );
+  }
+}
+
+let item2 = <Item message="Programmed" />;
+let item3 = <Item message="Went for a Walk" />;
+let item4 = <Item message="Ate Vitamins" />;
+
+let allTheThings = [item2, item3, item4];
+
+class ItemList extends React.Component {
+  constructor (props){
+    super ();
+  }
+  render (){
+    let items = allTheThings.map(thing => thing);
+    return (
+        <h4>{items}</h4>
+    );
+  }
+}
+
+class ItemCount extends React.Component {
+  constructor (props){
+    super ();
+  }
+  render (){
+    return (
+      <h4>There are {this.props.count} items on your list</h4>
+    );
+  }
+}
+
+class AppJumbotron extends React.Component {
+  render (){
+    return (
+      <div className="jumbotron">
+        <h2>{this.props.title}</h2>
+      </div>
+    );
+  }
+}
+
+class AppFooter extends React.Component {
+  render (){
+    return (
+      <div className="text-muted">
+        <small>&copy; {new Date().getFullYear()}</small>
+      </div>
+    );
+  }
+}
+
+let target = document.getElementById('app');
+
+
 
 export default App;
