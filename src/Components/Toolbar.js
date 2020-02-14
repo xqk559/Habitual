b2109682df2d5b619ea631a846fad9a44a960371
keith <xqk559@gmail.com>
2020-02-14 07:26:36 -0500

    Added more CSS styling

--- src/Components/Toolbar.js
+++ src/Components/Toolbar.js
@@ -5,14 +5,17 @@ const toolbar = () => {
     return (
         <div>
             <ul className="toolbar">
-                <li className="link"><a href="default.asp">
+                <li className="link"><a href="home.asp">
                     Home</a>
                 </li>
-                <li className="link"><a href="news.asp">
+                <li className="link"><a href="checklist.asp">
                     Checklist</a>
                 </li>
-                <li className="link"><a href="contact.asp">
+                <li className="link"><a href="stats.asp">
                     Statistics</a>
+                </li>
+                <li className="rightLink"><a href="login.asp">
+                    Login</a>
                 </li>
             </ul>
         </div>
