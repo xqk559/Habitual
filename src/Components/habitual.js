4fc3b5cbe52acc3ccda6c3f64a15d05c31751f5b
keith <xqk559@gmail.com>
2020-03-15 09:13:28 -0400

    Redux page now has add item button that maps a checklist from redux store

--- src/Components/habitual.js
+++ src/Components/habitual.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import * as actionCreators from '../Store/actions/index';
+import Item from './item';
 
 const Habitual = props => {
     return (
-        <div className="centered">
-            <button onClick={ () => props.addItem() }
-                type="button" 
-                className="btn btn-outline-dark btn-sm">
-                Add Item
-            </button>
-            {console.log(props.listReducer.items)}
+        <div>
+            <div className="centered">
+                <button onClick={ () => props.addItem() }
+                    type="button" 
+                    className="btn btn-outline-dark btn-sm">
+                    Add Item
+                </button>
+                {props.listReducer.items}
+            </div>
+            <ul>
+            {
+            props.listReducer.items.map((val, index) => {
+                return (
+                <li key={index}
+                    className="none">
+                    { <Item /> }
+                </li>
+                );
+            })
+            }
+            </ul>
         </div>
     );
 }
@@ -23,7 +38,7 @@ const mapStateToProps = state => {
   
 const mapDispatchToProps = dispatch => {
     return {
-        addItem: () => dispatch(actionCreators.addItem('cock')),
+        addItem: () => dispatch(actionCreators.addItem(<Item/>)),
     };
   };
   
---
+++ src/Components/item.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Item = props => {
+    return (
+        <div>
+          <div className="col-md-12">
+            <input type="checkbox" 
+                    />
+                &nbsp;FUCKSTAIN
+                <hr className="hr"/>
+          </div>
+        </div>
+    );
+}
+
+export default Item;
