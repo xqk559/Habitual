823cab0a22d0d35748ac58bd9103d2aa95b6bde1
keith <xqk559@gmail.com>
2020-03-19 08:16:34 -0400

    Now logged in on stats page

--- src/Components/stats.js
+++ src/Components/stats.js
@@ -1,15 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import '../App.css';
 import axios from 'axios';
+import {connect} from 'react-redux';
+import * as actionCreators from '../Store/actions/index';
 
 let dt = new Date();
 let utcDate = dt.toUTCString();
 
-const Statistics = () => {
+const Statistics = props => {
     let [lastDay, setLastDay] = useState();
 
     useEffect(()=>{
         axios.get('https://habitual-f64a5.firebaseio.com/history'+localStorage.getItem('userId')+'.json')
+            .then((response)=>setLastDay(response.data))
+            .then(console.log(lastDay))
+    }, [])
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token');
+        if(token){
+        props.signUpRedux(localStorage.getItem('token'),
+                           localStorage.getItem('userId'),
+                           localStorage.getItem('email'))
+        }
     }, [])
     
     return (
@@ -19,4 +32,16 @@ const Statistics = () => {
     )
 }
 
-export default Statistics;
+const mapStateToProps = state => {
+    return {
+        loginReducer: state.loginReducer,
+    };
+  };
+
+  const mapDispatchToProps = dispatch => {
+    return {
+        signUpRedux: (token, userId, email)=> dispatch(actionCreators.signUp(token, userId, email)),
+    };
+  };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Statistics);
