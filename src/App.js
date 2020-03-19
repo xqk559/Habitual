d5fbb31a214b9be71d10cb1ddf23f193f9ac8ef5
keith <xqk559@gmail.com>
2020-03-19 08:05:59 -0400

    Created new stat page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Route, BrowserRouter, Switch} from 'react-router-dom';
 import Stats from './Containers/Stats/Stats';
 import Habitual from './Components/habitual';
 import Auth from './Components/auth';
+import Statistics from './Components/stats';
 
 const App = () => {
     return (
@@ -33,13 +34,22 @@ const App = () => {
                             <Toolbar/>
                             <Auth />
                            </div>}/>
-        <Route path="/statistics/" 
+        <Route path="/oldstatistics/" 
                exact 
                render=
                 {()=>
                   <div>
                     <Toolbar/>
                     <Stats/>
+                  </div> 
+                }/>
+                <Route path="/statistics/" 
+               exact 
+               render=
+                {()=>
+                  <div>
+                    <Toolbar/>
+                    <Statistics/>
                   </div> 
                 }/>
       </Switch>
--- src/Components/stats.js
+++ src/Components/stats.js
@@ -1,1 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import '../App.css';
+import axios from 'axios';
+
+let dt = new Date();
+let utcDate = dt.toUTCString();
+
+const Statistics = () => {
+    let [lastDay, setLastDay] = useState();
+
+    useEffect(()=>{
+        axios.get('https://habitual-f64a5.firebaseio.com/history'+localStorage.getItem('userId')+'.json')
+    }, [])
+    
+    return (
+        <div className="stats">
+            <div>It is currently {utcDate}</div>
+        </div>
+    )
+}
+
+export default Statistics;
