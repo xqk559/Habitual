edb67435d6ce2093c25e854b44dab026ce929ce3
keith <xqk559@gmail.com>
2020-03-23 11:44:18 -0400

    Added redirect on signup

--- src/Components/auth.js
+++ src/Components/auth.js
@@ -3,131 +3,155 @@ import '../App.scss';
 import axios from 'axios';
 import {connect} from 'react-redux';
 import * as actionCreators from '../Store/actions/index';
+import {useHistory} from 'react-router-dom';
 
 const Auth = props => {
 
-    let [title, setTitle] = useState("Sign Up");
-    let [email, setEmail] = useState();
-    let [password, setPassword] = useState();
-    let [error, setError] = useState(null);
-    let [update, setUpdate] = useState();
+  let [title, setTitle] = useState("Sign Up");
+  let [email, setEmail] = useState();
+  let [password, setPassword] = useState();
+  let [error, setError] = useState(null);
+  let [update, setUpdate] = useState();
 
-    const authData = {
-        email: email,
-        password: password,
-        returnSecureToken: true
-    };
+  let history = useHistory();
 
-    useEffect(()=>{
-        const token = localStorage.getItem('token');
-        if(token){
-        props.signUpRedux(localStorage.getItem('token'),
-                          localStorage.getItem('userId'),
-                          localStorage.getItem('email'))
-        }
-    }, [])
+  const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+  };
 
-    const signUp = () => {
-        setError(null);
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBaJnlnubPwKJ9WkUJI6szWkCF_b0OomDk';
-        let axiosResponse;
-        const post = axios.post(url, authData)
-            .then(response=>axiosResponse=response.data)
-            .then(()=>localStorage.setItem('token', axiosResponse.idToken))
-            .then(()=>localStorage.setItem('userId', axiosResponse.localId))
-            .then(()=>localStorage.setItem('email', authData.email))
-            .catch(err=>setError(err.response.data.error))
-            .then(()=>{if(localStorage.getItem('token')){
+  useEffect(()=>{
+      const token = localStorage.getItem('token');
+      if(token){
+      props.signUpRedux(localStorage.getItem('token'),
+                        localStorage.getItem('userId'),
+                        localStorage.getItem('email'))
+      }
+  }, [])
+
+  const signUp = () => {
+    setError(null);
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBaJnlnubPwKJ9WkUJI6szWkCF_b0OomDk';
+    let axiosResponse;
+    const post = axios.post(url, authData)
+        .then(response=>axiosResponse=response.data)
+        .then(()=>localStorage.setItem('token', axiosResponse.idToken))
+        .then(()=>localStorage.setItem('userId', axiosResponse.localId))
+        .then(()=>localStorage.setItem('email', authData.email))
+        .catch(err=>setError(err.response.data.error))
+        .then(()=>{if(localStorage.getItem('token')){
                     props.signUpRedux(localStorage.getItem('token'),
                                       localStorage.getItem('userId'),
-                                      localStorage.getItem('email'))
-                }})
-    }
-
-    const login = () => {
-        setError(null);
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBaJnlnubPwKJ9WkUJI6szWkCF_b0OomDk';
-        let axiosResponse;
-        const post = axios.post(url, authData)
-            .then(response=>axiosResponse=response.data)
-            .then(()=>localStorage.setItem('token', axiosResponse.idToken))
-            .then(()=>localStorage.setItem('userId', axiosResponse.localId))
-            .then(()=>localStorage.setItem('email', authData.email))
-            .catch(err=>setError(err.response.data.error))
-            .then(()=>{if(localStorage.getItem('token')){
-                props.signUpRedux(localStorage.getItem('token'),
-                                  localStorage.getItem('userId'),
-                                  localStorage.getItem('email'))
+                                      localStorage.getItem('email'));
+                    history.push("/")
             }})
-    }
+  }
 
-    const logout = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('userId');
-        localStorage.removeItem('email');
-        setUpdate('updated');
-        props.logoutRedux();
-    }
+  const login = () => {
+      setError(null);
+      let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBaJnlnubPwKJ9WkUJI6szWkCF_b0OomDk';
+      let axiosResponse;
+      const post = axios.post(url, authData)
+          .then(response=>axiosResponse=response.data)
+          .then(()=>localStorage.setItem('token', axiosResponse.idToken))
+          .then(()=>localStorage.setItem('userId', axiosResponse.localId))
+          .then(()=>localStorage.setItem('email', authData.email))
+          .catch(err=>setError(err.response.data.error))
+          .then(()=>{if(localStorage.getItem('token')){
+              props.signUpRedux(localStorage.getItem('token'),
+                                localStorage.getItem('userId'),
+                                localStorage.getItem('email'))
+          }})
+  }
 
-    const switchSignUpLogin = () => {
-        if (title === "Sign Up") {
-            setTitle("Login")
-        } else if (title === "Login") {
-            setTitle("Sign Up")
-        }
-    }
+  const logout = () => {
+      localStorage.removeItem('token');
+      localStorage.removeItem('userId');
+      localStorage.removeItem('email');
+      setUpdate('updated');
+      props.logoutRedux();
+  }
 
-    const errorChecker = () => {
-        if (error) {
-            return (
-                <div className="centered">
-                    Error: {error.message}
-                </div>
-            )
-        }
-    }
+  const switchSignUpLogin = () => {
+      if (title === "Sign Up") {
+          setTitle("Login")
+      } else if (title === "Login") {
+          setTitle("Sign Up")
+      }
+  }
 
-    return (
-        <div>
-            <h1 className="loginPage">{title}</h1>
-            <div className="loginButtons">
-                <input
-                    type="text"
-                    className="loginInput"
-                    placeholder="Email"
-                    onChange={event=>setEmail(event.target.value)}/>
-                <button onClick={title === "Sign Up"
-                                 ? ()=>signUp()
-                                 : ()=>login()}
-                        className="btn btn-outline-dark btn-sm"
-                        style={{width: 70}}>
-                            {title}
-                </button>
-            </div>
-            <div className="loginButtons">
-                <input
-                    type="password"
-                    className="loginInput"
-                    placeholder="Password"
-                    onChange={event=>setPassword(event.target.value)}/>
-                <button
-                    onClick={()=>logout()}
-                    className="btn btn-outline-danger btn-sm"
-                    style={{width: 70}}>
-                        Logout
-                </button>
-            </div>
-            <br/>
-            <div className="loginPage">
-            <button
-                onClick={()=>switchSignUpLogin()}
-                className="btn btn-outline-primary btn-sm">
-                    Switch Login/Signup
-            </button>
-            </div>
-            {errorChecker()}
-        </div>
-    );
+  const errorChecker = () => {
+      if (error) {
+          return (
+              <div className="centered">
+                  Error: {error.message}
+              </div>
+          )
+      }
+  }
+
+  const confirmPassword = () => {
+    if(title == "Sign Up"){
+      return <div className="loginButtons">
+               <input
+                  type="password"
+                  className="loginInput"
+                  placeholder="Confirm Password"
+                  onChange={event=>setPassword(event.target.value)}/>
+               <button
+                  onClick={()=>logout()}
+                  className="transparent"
+                  style={{width: 70}}>
+               </button>
+             </div>
+    }
+  }
+
+  return (
+      <div>
+          <h1 className="loginPage">{title}</h1>
+          <div className="loginButtons">
+              <input
+                  type="text"
+                  className="loginInput"
+                  placeholder="Email"
+                  onChange={event=>setEmail(event.target.value)}/>
+              <button onClick={title === "Sign Up"
+                                ? ()=>signUp()
+                                : ()=>login()}
+                      className="btn btn-outline-dark btn-sm"
+                      style={{width: 70}}>
+                          {title}
+              </button>
+          </div>
+          <div className="loginButtons">
+              <input
+                  type="password"
+                  className="loginInput"
+                  placeholder="Password"
+                  onChange={event=>setPassword(event.target.value)}/>
+              <button
+                  onClick={()=>logout()}
+                  className="btn btn-outline-danger btn-sm"
+                  style={{width: 70}}>
+                      Logout
+              </button>
+          </div>
+          <div className="loginButtons">
+            {confirmPassword()}
+          </div>
+          <br/>
+          <div className="loginPage">
+          <button
+              onClick={()=>switchSignUpLogin()}
+              className="btn btn-outline-primary btn-sm">
+                  Switch Login/Signup
+          </button>
+          </div>
+          {errorChecker()}
+      </div>
+  );
 }
 
 const mapStateToProps = state => {
